3ab12785eb3b81ac4b42a3085952863023423413
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-11-13 15:16:42 +0530

    [Abhishek] added to the UC6 branch

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,7 +3,6 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-//UC3
 function GetEmployeeWage(empCheck){
     switch(empCheck){
         case IS_FULL_TIME:
@@ -15,16 +14,25 @@ function GetEmployeeWage(empCheck){
             return 0;
     }
 }
-//UC5
+//UC6
 const MAX_WORKING_HOURS=160;
 const MAX_WORKING_DAYS=20;
 let totalWorkingDays=0;
 let totalWorkingHours=0;
+let dailyWageArr=new Array();
 let totalWage=0;
+function calculateWage(emphours){
+    return emphours*WAGE_PER_HOUR;
+}
 while(totalWorkingDays<=MAX_WORKING_DAYS && totalWorkingHours<=MAX_WORKING_HOURS){
     let empCheck=Math.floor(Math.random()*10)%3;
-    totalWorkingHours=GetEmployeeWage(empCheck);
+    totalWorkingHours+=GetEmployeeWage(empCheck);
     totalWorkingDays++;
-    totalWage+=totalWorkingHours*WAGE_PER_HOUR;
+    let emphours = GetEmployeeWage(empCheck);
+    let dailyWage= calculateWage(emphours);
+    dailyWageArr.push(dailyWage);
 }
+dailyWageArr.forEach(wage => {
+    totalWage=totalWage+wage;
+});
 console.log("Total wage is "+totalWage);
